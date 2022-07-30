cb7ae9ac37ec0dcf2bd392f3cf2bb2e6f3e2cfd6
KTomy7 <kisstomi7.kt@gmail.com>
2022-07-30 14:38:34 +0300

    Adding Basic Styles

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const campgroundSchema = new mongoose.Schema({
     title: String,
+    image: String,
     price: Number,
     description: String,
     location: String
--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,12 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({ 
-            title: `${sampel(descriptors)} ${sampel(places)}`, 
-            location: `${cities[random1000].city}, ${cities[random1000].state}`, 
+        const camp = new Campground({
+            title: `${sampel(descriptors)} ${sampel(places)}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            price: Math.floor(Math.random() * 20) + 10
         });
         await camp.save();
     }
